36ad91d9a0fc0c29e49813907182068b07721424
melmf96 <melmf96@gmail.com>
2014-03-05 14:47:10 -0800

    Comment

--- example02/js/main.js
+++ example02/js/main.js
@@ -1,35 +1,43 @@
+var weapons = [
+    {
+        type : 'knife',
+        killRating: 2
+    },
+
+    {
+        type : 'gun',
+        killRating: 5
+    },
+
+    {
+        type : 'bow',
+        killRating: "bad"
+    }
+
+
+];
+
+var weaponLength = weapons.length;
+
+
 $( document ).ready(function() {
 
-    var weapons = [
-        {
-            type : 'knife',
-            killRating: 2
-        },
+    function describeWeapon(weapon) {
 
-        {
-            type : 'gun',
-            killRating: 5
-        },
 
-        {
-            type : 'bow',
-            killRating: "bad"
-        }
-    ];
+        //use a saved length instead of calculating in each loop
+        for (var i = 0; i < weaponLength; i++) {
+            if (typeof weapon[i].killRating != "number") {
+                alert("Weapon type " + weapon[i].type + " has a non-numeric kill rating");
+            }
 
-    var weaponLength = weapons.length;
-    //use a saved length instead of calculating in each loop
-    for ( var i = 0; i < weaponLength; i++){
-        describeWeapon(weapons[i]);
-    }
-});
+            console.log("Your weapon is " + weapon[i].type);
+            console.log("Your kill rating is " + weapon[i].killRating);
 
-var describeWeapon = function(weapon){
+         };
+    };
+    describeWeapon(weapons);
+})
 
-    if (typeof weapon.killRating != "number"){
-        alert("Weapon type " + weapon.type + " has a non-numeric kill rating");
-    }
 
-    console.log("Your weapon is " + weapon.type);
-    console.log("Your kill rating is " + weapon.killRating);
-}
+
