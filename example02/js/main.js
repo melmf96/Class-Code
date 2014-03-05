534dd7e3f0b4c981dc325dff4eb96f975f0aea2a
melmf96 <melmf96@gmail.com>
2014-03-05 14:50:02 -0800

    Comment

--- example02/js/main.js
+++ example02/js/main.js
@@ -19,23 +19,23 @@ var weapons = [
 
 var weaponLength = weapons.length;
 
+function describeWeapon(weapon) {
+
+
+    //use a saved length instead of calculating in each loop
+    for (var i = 0; i < weaponLength; i++) {
+        if (typeof weapon[i].killRating != "number") {
+            alert("Weapon type " + weapon[i].type + " has a non-numeric kill rating");
+        }
+
+        console.log("Your weapon is " + weapon[i].type);
+        console.log("Your kill rating is " + weapon[i].killRating);
+
+    };
+};
 
 $( document ).ready(function() {
 
-    function describeWeapon(weapon) {
-
-
-        //use a saved length instead of calculating in each loop
-        for (var i = 0; i < weaponLength; i++) {
-            if (typeof weapon[i].killRating != "number") {
-                alert("Weapon type " + weapon[i].type + " has a non-numeric kill rating");
-            }
-
-            console.log("Your weapon is " + weapon[i].type);
-            console.log("Your kill rating is " + weapon[i].killRating);
-
-         };
-    };
     describeWeapon(weapons);
 })
 
