b0ea1258e21455cfbc112b52ce5f23afd7d26c37
melmf96 <melmf96@gmail.com>
2014-03-04 21:56:10 -0800

    Comment

--- example02/js/main.js
+++ example02/js/main.js
@@ -1,1 +1,35 @@
+$( document ).ready(function() {
 
+    var weapons = [
+        {
+            type : 'knife',
+            killRating: 2
+        },
+
+        {
+            type : 'gun',
+            killRating: 5
+        },
+
+        {
+            type : 'bow',
+            killRating: "bad"
+        }
+    ];
+
+    var weaponLength = weapons.length;
+    //use a saved length instead of calculating in each loop
+    for ( var i = 0; i < weaponLength; i++){
+        describeWeapon(weapons[i]);
+    }
+});
+
+var describeWeapon = function(weapon){
+
+    if (typeof weapon.killRating != "number"){
+        alert("Weapon type " + weapon.type + " has a non-numeric kill rating");
+    }
+
+    console.log("Your weapon is " + weapon.type);
+    console.log("Your kill rating is " + weapon.killRating);
+}
